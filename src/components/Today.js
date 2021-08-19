28fde10fd608aa9232d141aa5d8bd90595f97ea6
martbern <martinbernstorff@gmail.com>
2021-08-19 10:14:19 +0200

    Add sections to planning

--- src/components/Today.js
+++ src/components/Today.js
@@ -27,17 +27,35 @@ class Yesterday extends Component {
     return (
       <div>
         <HashRouter>
-        <ul className="grid grid-rows-2 grid-cols-3 gap-2 max-w-md">
-          
-          <li><NavLinkButton to="/today/Calendar" text="Calendar" /></li>
-          <li><NavLinkButton to="/today/Todoist" text="Todoist" /></li>
-          <li><NavLinkButton to="/today/Gmail" text="Gmail" /></li>
+        <div className="grid grid-cols-3 gap-4">
+          <div>
+            <h1 className="text-xs font-bold mt-4 text-gray-300 text-center">Most days</h1>
+            <div className="grid grid-cols-2 gap-1">  
+              <NavLinkButton to="/today/Calendar" text="Calendar" />
+              <NavLinkButton to="/today/ActivityListRecents" text="Recents" />
+              <NavLinkButton to="/today/Todoist" text="Todoist" />
+              <NavLinkButton to="/today/Gmail" text="Gmail" />
+            </div>
+          </div>
 
-          <li><NavLinkButton to="/today/ActivityListRecents" text="Recents" /></li>
-          <li><NavLinkButton to="/today/Taskheat" text="Taskheat" /></li>
-          <li><NavLinkButton to="/today/KernelList" text="Kernels" /></li>
-          <li><NavLinkButton to="/today/TaskList" text="Activity menu" /></li>
-        </ul>
+          <div>
+            <h1 className="text-xs font-bold mt-4 text-gray-300 text-center">Leisure</h1>
+            <div className="grid grid-cols-2 gap-1">  
+              <NavLinkButton to="/today/KernelList" text="Kernels" />
+              <NavLinkButton to="/today/TaskList" text="Activity menu" />
+            </div>
+          </div>
+
+          <div>
+            <h1 className="text-xs font-bold mt-4 text-gray-300 text-center">Work</h1>
+            <div className="grid grid-cols-2 gap-1">
+              <NavLinkButton to="/today/Taskheat" text="Slack" />
+              <NavLinkButton to="/today/Taskheat" text="Outlook" />
+              <NavLinkButton to="/today/Taskheat" text="Taskheat" />
+            </div>
+          </div>
+        </div>
+
 
         <Route path="/today/ActivityListRecents" component={ActivityListRecents}/>
         <Route path="/today/Calendar" component={Calendar}/>
